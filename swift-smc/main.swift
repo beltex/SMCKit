ba7807146e062891d6d81c85eeb49a6fd9fc7ac8
beltex <cse93251@cse.yorku.ca>
2014-08-06 13:44:27 -0400

    Successfully get CPU temp
    - When the bytes array is written to on the C side, it for some reason is
      not readable on the Swift side. This may or may not be a bug in the
      translation from C to Swift. The hack fix is to instead define 32 UInt8,
      in which case we can then read the data.

--- swift-smc/main.swift
+++ swift-smc/main.swift
@@ -66,7 +66,6 @@ struct SMCKeyInfoData {
     var dataAttributes : UInt8 = 0
 }
 
-
 struct SMCParamStruct {
     var key        : UInt32 = 0
     var vers       : SMCVersion = SMCVersion()
@@ -77,7 +76,41 @@ struct SMCParamStruct {
     var status     : UInt8 = 0
     var data8      : UInt8 = 0
     var data32     : UInt32 = 0
-    var btyes      = [UInt8](count: 32, repeatedValue: 0)
+    
+    // HACK ALERT: Can't read C array, so instead we enumerate it :)
+    
+    var bytes_0    : UInt8 = 0
+    var bytes_1    : UInt8 = 0
+    var bytes_2    : UInt8 = 0
+    var bytes_3    : UInt8 = 0
+    var bytes_4    : UInt8 = 0
+    var bytes_5    : UInt8 = 0
+    var bytes_6    : UInt8 = 0
+    var bytes_7    : UInt8 = 0
+    var bytes_8    : UInt8 = 0
+    var bytes_9    : UInt8 = 0
+    var bytes_10   : UInt8 = 0
+    var bytes_11   : UInt8 = 0
+    var bytes_12   : UInt8 = 0
+    var bytes_13   : UInt8 = 0
+    var bytes_14   : UInt8 = 0
+    var bytes_15   : UInt8 = 0
+    var bytes_16   : UInt8 = 0
+    var bytes_17   : UInt8 = 0
+    var bytes_18   : UInt8 = 0
+    var bytes_19   : UInt8 = 0
+    var bytes_20   : UInt8 = 0
+    var bytes_21   : UInt8 = 0
+    var bytes_22   : UInt8 = 0
+    var bytes_23   : UInt8 = 0
+    var bytes_24   : UInt8 = 0
+    var bytes_25   : UInt8 = 0
+    var bytes_26   : UInt8 = 0
+    var bytes_27   : UInt8 = 0
+    var bytes_28   : UInt8 = 0
+    var bytes_29   : UInt8 = 0
+    var bytes_30   : UInt8 = 0
+    var bytes_31   : UInt8 = 0
 }
 
 
@@ -95,6 +128,7 @@ service = IOServiceGetMatchingService(kIOMasterPortDefault,
 
 if (service == 0) {
     println("\(IOSERVICE_APPLESMC) NOT FOUND")
+    //return -1
 }
 
 result = IOServiceOpen(service, mach_task_self_, 0, &conn)
@@ -102,9 +136,11 @@ IOObjectRelease(service)
 
 if (result != kIOReturnSuccess) {
     println("FAILED TO OPEN IOService; \(result)")
+    //return -1
 }
 
 var r2 : kern_return_t
+
 
 var input  = SMCParamStruct()
 var output = SMCParamStruct()
@@ -119,11 +155,54 @@ input.data8 = 9
 
 println("SWIFT DATA8: \(input.data8)")
 
+
 r2 = IOConnectCallStructMethod(conn, 2, &input, inputStructCnt, &output, &outputStructCnt)
 
 
 if (r2 == kIOReturnSuccess) {
     println("WORKS")
+    //    println("BTYE ARRAY CHECK: \(input.bytes[0])")
+}
+else {
+    println("///////////////////////////")
+    println("NOOO")
+    println((r2>>26)&0x3f)
+    println((r2>>14)&0xfff)
+    println(r2 & 0x3fff)
+}
+
+
+var dataSize = output.keyInfo.dataSize
+
+input  = SMCParamStruct()
+output = SMCParamStruct()
+input.key = 1413689412
+input.keyInfo.dataSize = dataSize
+input.data8 = 5
+
+
+// Overwritten - first one is, second is to denote size of struct returned from AppleSMC
+inputStructCnt = 80
+outputStructCnt = 80
+
+var ans = [UInt8](count: 32, repeatedValue: 0)
+
+r2 = IOConnectCallStructMethod(conn, 2, &input, inputStructCnt, &output, &outputStructCnt)
+
+
+if (r2 == kIOReturnSuccess) {
+    println("WORKS")
+    //    println("TEMP: \(output.bytes)")
+    //    println("SIZEOF VALUE \(sizeofValue(output.bytes))")
+    //    r2 = IOConnectCallStructMethodWrapper(conn, 2, &input, inputStructCnt, &output, &outputStructCnt)
+    //    println("WORKS")
+    //    var p = UnsafeBufferPointer(start: &output.btyes, length: 32)
+    
+    
+    //    memcpy(&ans, output.btyes, 32)
+    //    var test = UnsafeBufferPointer(start: &output.btyes, length: 32)
+    var intVal = Int(output.bytes_0) * Int(256) + Int(output.bytes_1)
+    println("TEMP: \(Double(intVal / 4) / 64.0)")
 }
 else {
     println("///////////////////////////")
