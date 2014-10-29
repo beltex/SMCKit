1a4050b4122f1f34bcdb1f85aa46938fc9769f7d
beltex <cse93251@cse.yorku.ca>
2014-10-29 16:09:23 -0400

    Add public init() since project is now a framework
    - See here for why exactly - http://stackoverflow.com/a/25598333
    - Correct framework version (0.0.7)
    - Add back debug compiler flag

--- SMCKit/SMCKit.h
+++ SMCKit/SMCKit.h
@@ -1,4 +1,23 @@
-
+/*
+ * SMCKit.h
+ * SMCKit
+ *
+ * Copyright (C) 2014  beltex <https://github.com/beltex>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #import <Cocoa/Cocoa.h>
 
@@ -8,6 +27,5 @@ FOUNDATION_EXPORT double SMCKitVersionNumber;
 //! Project version string for SMCKit.
 FOUNDATION_EXPORT const unsigned char SMCKitVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <SMCKit/PublicHeader.h>
-
-
+// In this header, you should import all the public headers of your framework
+// using statements like #import <SMCKit/PublicHeader.h>
