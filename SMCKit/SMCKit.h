69d306bb8d42e121fb9ab4dc0372bb7fcb85b9a0
beltex <cse93251@cse.yorku.ca>
2014-10-29 00:34:07 -0400

    Convert Xcode project to Cococa framework
    - Stock test file added

---
+++ SMCKit/SMCKit.h
@@ -0,0 +1,13 @@
+
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SMCKit.
+FOUNDATION_EXPORT double SMCKitVersionNumber;
+
+//! Project version string for SMCKit.
+FOUNDATION_EXPORT const unsigned char SMCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMCKit/PublicHeader.h>
+
+
